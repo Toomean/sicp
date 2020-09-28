3447f0e128addb2f49a347ee0b9b9085a3afc956
Artem Tumin <arttumin@gmail.com>
2020-09-29 00:21:17 +0300

    chapter1: add solution for exercise 1.3

---
+++ sicp/chapter1/03.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (solution x y z)
+  (define biggest1 x)
+  (define biggest2 y)
+  (cond
+    (
+      (and (> z biggest1) (> z biggest2)) (
+        (set! biggest1 z)
+        (set! biggest2
+          (if (> x y)
+            x
+            y
+          )
+        )
+      )
+    )
+    (
+      (> z biggest1) (set! biggest1 z)
+    )
+    (
+      (> z biggest2) (set! biggest2 z)
+    )
+  )
+
+  (+
+    (* biggest1 biggest1)
+    (* biggest2 biggest2)
+  )
+)
+
+(check-equal? (solution 3 2 1) 13)
+(check-equal? (solution 5 1 3) 34)
+(check-equal? (solution 0 5 -1) 25)
+(check-equal? (solution 100 1 1) 10001)
+(check-equal? (solution 0 5 2) 29)
