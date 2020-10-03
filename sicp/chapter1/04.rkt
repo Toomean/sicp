13781afb0e99470468a6db0676f524d56f75c199
Artem Tumin <arttumin@gmail.com>
2020-10-03 18:27:48 +0300

    chapter1: add solution for exercise 1.4

---
+++ sicp/chapter1/04.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(#%require rackunit)
+
+#| a-plus-abs-b
+#| It adds an absolute value of b to a
+
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + - ) a b))
+
+(check-equal? (a-plus-abs-b 1 1) 2)
+(check-equal? (a-plus-abs-b 1 0) 1)
+(check-equal? (a-plus-abs-b 1 -1) 2)
