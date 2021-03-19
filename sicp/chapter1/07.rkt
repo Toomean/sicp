f03a1786e4f29ffe4d9dc3685667c51d02bd2b51
Artem Tumin <arttumin@gmail.com>
2021-03-19 19:48:24 +0300

    chapter1: add exercise 0.7

---
+++ sicp/chapter1/07.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+(define (improve guess x) (average guess (/ x guess)))
+
+(define (good-enough? guess oldguess) 
+   (< (abs (- guess oldguess)) 
+      (* guess 0.001)))
+
+(define (sqrt-iter guess oldguess x) 
+   (if (good-enough? guess oldguess) 
+       guess 
+       (sqrt-iter (improve guess x) guess 
+                  x)))
+
+(define (sqrt x) 
+   (sqrt-iter 1.0 2.0 x)) 
+
+; Testing sqrt procedure
+(check-equal? (sqrt 4) 1)
